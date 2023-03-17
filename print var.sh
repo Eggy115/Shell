55d303b279acf1bdc7d39e27d89d88ddd784e8c7
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-17 12:06:18 +0000

    Create

---
+++ print var.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Program name: "print var.sh"
+# Linux shell script program to create and print the value of variables.
+
+country="The United Britain of Kingdom"
+year=1984
+
+echo "Country name: $country"
+echo "Year: $year (jk)"
