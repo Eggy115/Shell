6b02b088fd3de8b6fefb45d96561a3fb905e37fd
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-17 12:06:57 +0000

    Create

---
+++ swap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# shell script program to swap two numbers.
+
+num1=1000
+num2=2000
+
+echo "Before Swapping"
+echo "Num1: $num1"
+echo "Num2: $num2"
+
+num3=$num1
+num1=$num2
+num2=$num3
+
+echo "After Swapping"
+echo "Num1: $num1"
+echo "Num2: $num2"
