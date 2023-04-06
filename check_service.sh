63524b85399a64a9071b18e02360d2c3a16f976a
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-06 12:42:08 +0100

    Add files via upload

--- check_service.sh
+++ check_service.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-if [ -z $1 ];then
-    echo "错误:未输入服务名称."
-    echo "用法:脚本名 服务器名称."
-    exit
-fi
-if systemctl is-active $1 &>/dev/null ;then
-    echo "$1已经启动..."
-else
-    echo "$1未启动..."
-fi
-if systemctl is-enabled $1 &>/dev/null ;then
-    echo "$1是开机自启动项."
-else
-    echo "$1不是开机自启动项."
-fi
+#功能描述(Description):使用函数检查服务是否启动的案例脚本.
+
+date_time=$(date +'%Y-%m-%dT%H:%M:%S%z')
+
+function check_services() {
+    for i in "$@"
+    do
+        if systemctl --quiet is-active ${i}.service; then
+            echo -e "[$date_time)]: \033[92mservice $i is active\033[0m"
+        else
+            echo "[$date_time]: service $i is not active" >&2
+        fi
+    done
+}
+
+check_services httpd sshd vsftpd
