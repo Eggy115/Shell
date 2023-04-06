98aded2ad9c96cd6e37eaea442140fd8cd1aaef1
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-06 12:54:21 +0100

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-sp='/-\|'
-printf ' '
-while true; do
-    printf '\b%.1s' "$sp"
-    sp=${sp#?}${sp%???}
-    sleep 0.1
+CWD="$(dirname "$0")"
+
+. ./perform_db_operation.sh
+
+#Call the runQuery function from the perform_db_operation.sh file
+tables=$(runQuery "show tables")
+
+for table in "${tables[@]}"
+do
+   echo "$table"
+done
+
+#Get all the rowd from a table
+all_vals=$(runQuery "select * from account")
+
+for val in "${all_vals[@]}"
+do
+   echo "$val"
 done
